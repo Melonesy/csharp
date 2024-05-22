bc1a2b6131dde86012b2d6185d23fd5c2ee34c63
Jose Manuel Arevalo <jose@prueba.es>
2024-05-22 11:28:09 +0200

    Commit 1

--- Formacion.CSharp.Ejercicios.ConsoleApp1/Program.cs
+++ Formacion.CSharp.Ejercicios.ConsoleApp1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        
     }
+
+    // Pedir un número por consola y analizarlo para retorna uno de los dos mensajes:
+    // -> El número introducido es [valor]
+    // -> No se ha introducido un número o no se puede convertir
+
+
+    //
+
 }
